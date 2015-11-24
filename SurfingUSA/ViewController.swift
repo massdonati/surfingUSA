64944d30e331d23f3732218b836f89cca85b365d
Massimo Donati <mass.donati@gmail.com>
2015-11-23 23:46:47 -0800

    Initial Commit

---
+++ SurfingUSA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SurfingUSA
+//
+//  Created by massimo on 23/11/15.
+//  Copyright © 2015 Team84. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
